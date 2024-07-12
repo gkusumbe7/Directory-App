ad49f1909c3b6bb93cb2748acc5862d5509e7182
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-07-12 23:10:30 +0530

    added code

--- src/component/DefaultTable.jsx
+++ src/component/DefaultTable.jsx
@@ -1,7 +1,6 @@
-import { Card, Typography } from "@material-tailwind/react";
- 
+// import { useState } from "react";
+import { Card, Typography } from "@material-tailwind/react"; 
 const TABLE_HEAD = ["Name", "DOB", "Aadhar Number", "Mobile Number" , "Age" , "Action" ];
- 
 const TABLE_ROWS = [
   {
     name: "John Michael",
@@ -13,9 +12,17 @@ const TABLE_ROWS = [
 ];
  
 export default function DefaulTable() {
+
+  // const [personName , setPersonName] = useState("");
+  // const [ dateOfBirth , setDateOfBirth ] = useState("");
+  // const [personAge , setPersonAge] = useState("");
+  // const [personAadhar , setPersonAadhar] = useState("");
+  // const [personMobile , setPersonMobile ] = useState("");
+  
+
   return (
     <>   
-    <Card className="h-full w-full overflow-scroll">
+    <Card className="h-full w-full overflow-scroll mt-10">
     <Card >
       <table className="w-full min-w-max table-auto text-left">
         <thead className="border border-black">
@@ -46,6 +53,7 @@ export default function DefaulTable() {
                 <Typography variant="small" color="blue-gray" className="font-normal">
                   {dateOfBirth}
                 </Typography>
+                
               </td>
               <td className="p-2">
                 <Typography variant="small" color="blue-gray" className="font-normal">
@@ -72,29 +80,29 @@ export default function DefaulTable() {
         </tbody>
       </table>
     </Card>
-        <span className="text-center font-md text-orange-500 font-semibold"> 
+        <span className="text-center font-md text-orange-500 font-semibold "> 
             Fill below form for New Entry
        </span>
     <Card>
-    <form >
-        <tr key={name} className="border-blue-gray-100 flex gap-8 text-center px-4 text-[15px]">
+    <form onSubmit={addPersonInfo}>
+        <tr key={name} className="border-blue-gray-100 flex gap-8 text-center px-4 text-[15px] p-2 ">
         <td>
-          <input type="text" placeholder="Enter Name"/>
+          <input type="text" id="personNameId" placeholder="Enter Name" className="border border-black" />
         </td>
         <td>
-          <input type="date" />
+          <input type="date" id="dateId" />
         </td>
         <td>
-          <input type="text" placeholder="Enter Aadhar" />
+          <input type="text" id="aadharId" placeholder="Enter Aadhar" />
         </td>
         <td>
-          <input type="text" placeholder="Enter Mobile Number" />
+          <input type="text" id="mobileNumberId" placeholder="Enter Mobile Number" />
         </td>
         <td>
-          <input type="text" placeholder="Enter Age" />
+          <input type="text" id="ageId" placeholder="Enter Age" />
         </td>
         <table>
-          <button>Save</button>
+          <button type="submit">Save</button>
         </table>
         </tr>
         </form>
